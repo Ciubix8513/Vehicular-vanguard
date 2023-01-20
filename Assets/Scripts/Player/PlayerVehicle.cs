2443d1650e63a689a15521439b0035a61e91e92e
Luna <ciubix8514@gmail.com>
2023-01-20 23:24:33 +0300

    Added reset position button

--- Assets/Scripts/Player/PlayerVehicle.cs
+++ Assets/Scripts/Player/PlayerVehicle.cs
@@ -1,9 +1,24 @@
+using System.Linq;
 using CarGame.Vehicle;
+using CarGame.Vehicle.Saving;
 using UnityEngine;
+
 namespace CarGame.Player
 {
     public class PlayerVehicle : MonoBehaviour
     {
         public Part Root;
+        private TransformData _initialPosition;
+        void Awake() => _initialPosition = new TransformData(transform);
+
+        public void ResetTransform()
+        {
+            transform.Load(_initialPosition);
+            GetComponentsInChildren<Rigidbody>().ToList().ForEach(_ =>
+            {
+                _.velocity = Vector3.zero;
+                _.angularVelocity = Vector3.zero;
+            });
+        }
     }
 }
