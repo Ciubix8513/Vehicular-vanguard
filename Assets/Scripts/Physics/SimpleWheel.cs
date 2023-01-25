a397accddc94d6779be63f2b101602b93e7d06b2
Luna <ciubix8514@gmail.com>
2023-01-25 23:48:59 +0300

    Added wheel offset calculation function

--- Assets/Scripts/Physics/SimpleWheel.cs
+++ Assets/Scripts/Physics/SimpleWheel.cs
@@ -1,11 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class SimpleWheel : MonoBehaviour
 {
+    public Vector3 WheelSize = Vector3.one;
+    public float Torque = 0.0f;
+    public float BreakTorque = 0.0f;
+    //The axis of rotation
+    public Vector3 RotationAxis = Vector3.up;
+    public float MaxRotation = 10.0f;
+    public float TargetRotation = 0.0f;
+    public float RotationForce = 1.0f;
     private Rigidbody _rigidbody;
     void Awake() => _rigidbody = GetComponent<Rigidbody>();
-    
+
+    /// <summary>
+    /// Calculates The space needed for a wheel to rote properly
+    /// </summary>
+    /// <returns></returns>
+    public float CalculateWheelOffset()
+    {
+        float y = WheelSize.z / 2;
+        float x = WheelSize.x / 2;
+        float alpha = Mathf.Deg2Rad * MaxRotation;
+        float b = Mathf.Sin(alpha) * y;
+        float d = Mathf.Cos(alpha- Mathf.PI) * x;
+
+        return b + d;
+    }
+
+    void FixedUpdate()
+    {
+        //Use force for motor and breaks
+        //Use Impulse for rotation 
+        // _rigidbody.AddTorque())
+        // Quaternion.
+    }
 }
--- Assets/Scripts/Vehicle/Parts/ExperimentalWheel.cs
+++ Assets/Scripts/Vehicle/Parts/ExperimentalWheel.cs
@@ -4,12 +4,20 @@ namespace CarGame.Vehicle.Parts
 {
     public class ExperimentalWheel : Part
     {
+        SimpleWheel wheel;
+        private void Awake()
+        {
+            wheel = GetComponent<SimpleWheel>();
+            wheel.WheelSize = m_size;
+        }
+
         public override void PartConnect(Part other)
         {
             Joint = gameObject.AddComponent<ConfigurableJoint>();
             Joint.connectedBody = other.Rigidbody;
             var j = (ConfigurableJoint)Joint;
             j.xMotion = j.yMotion = j.zMotion = j.angularZMotion = j.angularYMotion = ConfigurableJointMotion.Locked;
+            // j.anchor = 
         }
     }
 }
