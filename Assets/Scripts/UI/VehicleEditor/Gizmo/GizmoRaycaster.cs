615611d8d95baa74225c24bcb940c6dfcaec7044
Luna <ciubix8514@gmail.com>
2023-02-28 00:05:16 +0300

    Improved editor rotation to be more intuitive

--- Assets/Scripts/UI/VehicleEditor/Gizmo/GizmoRaycaster.cs
+++ Assets/Scripts/UI/VehicleEditor/Gizmo/GizmoRaycaster.cs
@@ -21,12 +21,16 @@ namespace VehicularVanguard.Vehicle.Editor
 {
     public class GizmoRaycaster : MonoBehaviour
     {
+        enum Axis
+        {
+            x, y, z
+        }
         public static bool Rotating = false;
         private static GameObject s_rotation;
         private static Camera s_camera;
         [SerializeField]
         private GameObject _rotationPrefab;
-        private static int s_axis;
+        private static Vector3 s_axis;
         private static Part s_rotatingObj;
         public static int RotatingObjectId
         {
@@ -47,8 +51,8 @@ namespace VehicularVanguard.Vehicle.Editor
         public void SwitchMode(int mode)
         {
             Rotating = false;
-            if(s_rotation)
-            s_rotatingObj?.RestoreProxiesLayers();
+            if (s_rotation)
+                s_rotatingObj?.RestoreProxiesLayers();
             switch (mode)
             {
                 case 1:
@@ -67,44 +71,46 @@ namespace VehicularVanguard.Vehicle.Editor
         public static void Raycast()
         {
             if (InputManager.editorMode != EditorMode.rotate) return;
-            if (!Physics.Raycast(s_camera.ScreenPointToRay(Input.mousePosition), out var hit, 500.0f)) return;
-
+            if (Physics.Raycast(s_camera.ScreenPointToRay(Input.mousePosition), out var hit, 500.0f, 1 << 8))
+            {
+                if (!hit.collider.CompareTag("Gizmo")) goto ray2;
+                Rotating = true;
+                if (hit.collider.name.ToLower() == "x")
+                    s_axis = Vector3.right;
+                else if (hit.collider.name.ToLower() == "y")
+                    s_axis = Vector3.up;
+                else
+                    s_axis = Vector3.forward;
+                Rotate(s_axis);
+            }
+        ray2:
+            if (!Physics.Raycast(s_camera.ScreenPointToRay(Input.mousePosition), out hit, 500.0f)) return;
             if (hit.collider.CompareTag("Part"))
             {
                 SetRotatingObject(hit.collider.gameObject);
                 return;
             }
-            if (!hit.collider.CompareTag("Gizmo")) return;
-            Rotating = true;
-            if (hit.collider.name.ToLower() == "x")
-                s_axis = 0;
-            else if (hit.collider.name.ToLower() == "y")
-                s_axis = 1;
-            else
-                s_axis = 2;
-            Rotate(s_axis);
         }
         public static void SetRotatingObject(GameObject o)
         {
-            if(s_rotatingObj != null)
+            if (s_rotatingObj != null)
                 s_rotatingObj?.RestoreProxiesLayers();
             s_rotatingObj = o.GetComponent<PartProxy>().part;
             s_rotatingObj.SaveProxiesLayers();
             s_rotatingObj.SetProxiesLayer(2);
             s_rotation.transform.position = o.transform.position;
-            s_rotation.transform.rotation = o.transform.rotation;
+            // s_rotation.transform.rotation = o.transform.rotation;
             s_rotation.SetActive(true);
         }
-        static void Rotate(int axis)
+        static void Rotate(Vector3 axis)
         {
             var parts = s_rotatingObj.attachedParts;
             parts[s_rotatingObj.parentFace] = false;
-            if (axis == 0)
-                s_rotatingObj.transform.Rotate(new Vector3(Input.GetKey(KeyCode.LeftShift) ? -90 : 90, 0, 0));
-            else if (axis == 1)
-                s_rotatingObj.transform.Rotate(new Vector3(0, Input.GetKey(KeyCode.LeftShift) ? -90 : 90, 0));
-            else
-                s_rotatingObj.transform.Rotate(new Vector3(0, 0, Input.GetKey(KeyCode.LeftShift) ? -90 : 90));
+
+            //THANK YOU CHATGPT
+            s_rotatingObj.transform.rotation = 
+            Quaternion.AngleAxis(Input.GetKey(KeyCode.LeftShift) ? -90 : 90, axis) 
+            * s_rotatingObj.transform.rotation;
             //This is a pretty dumb fix but it should work
             var body = s_rotatingObj.Joint.connectedBody;
             Destroy(s_rotatingObj.Joint);
